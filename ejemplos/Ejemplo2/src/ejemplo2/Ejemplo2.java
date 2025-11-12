7571522feee29b44c235e52315d32442127e73ce
danyberna2002@gmail.com <danyberna2002@gmail.com>
2025-11-12 12:19:59 -0500

    ejemplo2

--- ejemplos/Ejemplo2/src/ejemplo2/Ejemplo2.java
+++ ejemplos/Ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -28,14 +28,23 @@ public class Ejemplo2 {
         entrada.useLocale(Locale.US); // 10,2 / 10.2
         double promedio;
         String miNombre;
+        double edad;
+        String ciudad;
         System.out.println("Ingrese el nombre por favor");
         miNombre = entrada.nextLine();
         System.out.println("Ingrese el promedio por favor");
         promedio = entrada.nextDouble();
+        System.out.println("Ingrese la edad por favor");
+        edad = entrada.nextDouble();
+        entrada.nextLine();
+        System.out.println("Ingrese nombre de la ciudad por favor");
+        ciudad = entrada.nextLine();
+        
 
         if (promedio >= 7.5) {
-            System.out.printf("Estudiante %s aprobado con un promedio: %.2f\n", 
-                    miNombre, promedio);
+            System.out.printf("Estudiante %s edad %s ciudad %s aprobado con un "
+                    + "promedio: %.2f\n", 
+                    miNombre, edad, ciudad, promedio);
         }else{
             System.out.printf("Estudiante reprobado con un "
                     + "promedio: %.2f\n", promedio);
