b24e4a814cde097c521118f61d1ec40bdf4ebcd0
danyberna2002@gmail.com <danyberna2002@gmail.com>
2025-11-12 11:56:35 -0500

    problema011

---
+++ ejemplos/Ejemplo1/src/ejemplo1/Ejemplo11.java
@@ -0,0 +1,54 @@
+/*
+ * Realizar un programa en java que permita determinar
+ * si un estudiante ha pasado el ciclo
+ * se ingresa el promedio del estudiante
+ * - Un estudiante pasa el ciclo si tiene un promedio
+ * mayor o igual a 7.5. Si el estudiante aprueba el ciclo, presentar
+ * un mensaje como sigue: Estudiante aprobado con un promedio: 8.1
+ */
+package ejemplo1;
+ import java.util.Scanner;
+ import java.util.Locale;
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo11 {
+
+    public static void main(String[] args) {
+        // Condicionales simplesmain
+        Scanner entrada = new Scanner (System.in);
+        entrada.useLocale(Locale.US);
+        
+        double promedio = 7.5;
+        
+        /*
+        System.out.println("Ingrese un valor para analizar");
+        promedio = entrada.nextDouble();
+        */
+        
+
+        
+        System.out.println("Ingrese un valor para analizar");
+        promedio = entrada.nextDouble();
+
+         System.out.println("Ingrese limite de notas");
+        promedio = entrada.nextDouble();
+
+        if (promedio >= 7.5) {
+            System.out.printf("Estudiante aprobado con un "
+                    + "promedio: %.2f\n", promedio);
+        }
+
+        if (promedio == 7.5) {
+            System.out.printf("Estudiante aprobado con un "
+                    + "promedio: %.2f\n", promedio);
+        }
+
+        if (promedio < 7.5) {
+            System.out.printf("Estudiante aprobado con un "
+                    + "promedio: %.2f\n", promedio);
+        }
+    }
+
+}
