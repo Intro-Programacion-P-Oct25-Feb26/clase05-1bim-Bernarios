6d846f56dc09680c7b58c6f7b2b34fd34ce9da51
danyberna2002@gmail.com <danyberna2002@gmail.com>
2025-11-13 11:34:40 -0500

    ejemplo7

---
+++ ejemplos/Ejemplo4/src/ejemplo4/Ejemplo7.java
@@ -0,0 +1,42 @@
+/*
+El programa presenta el mensaje de "acceso correcto" para las placas de 
+vehiculos de las provincias del Guayas y el Pichincha 
+ */
+package ejemplo4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo7 {
+
+    public static void main(String[] args) {
+        // 
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        System.out.println("Ingrese la placa");
+        String ciudad = entrada.nextLine();
+
+        // L    0
+        // O    1
+        // J    2 
+        // A    3
+        // 
+        String inicial = ciudad.substring(0, 1); // se obtiene 
+        // el primer 
+        // caracter de la cadena
+        // Si el valor de ciudad es 
+        // "Loja" se obtiene "L"
+
+        if ( inicial.equals("g") || inicial.equals("G") || inicial.equals("p")
+                || inicial.equals("P")) {
+            System.out.printf("%s\n", "acceso correcto");
+        } else {
+            System.out.printf("%s\n", "acceso incorrecto");
+        }
+
+    }
+}
